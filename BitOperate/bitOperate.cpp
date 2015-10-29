b15f99b4821ad124b7037b3c77db60932a32b9d6
administratoroot <zhxun0422@gmail.com>
2015-10-29 09:37:58 +0800

    提交bitoperate

---
+++ BitOperate/bitOperate.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int i = 5;//101
+	int j = 2;//010
+	int k = 6;//110
+	cout << "(i&j)" << (i & j) << endl;//0
+	cout << "(i|j)" << (i | j) << endl;//111
+	cout << "(i^j)" << (i ^ j) << endl;//111
+	cout << "(i^k)" << (i ^ k) << endl;//011
+	cout << "(~i)" << (~i) << endl;//
+	system("pause");
+	return 0;
+}
