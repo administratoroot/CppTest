011291adb55178368de6f967da67bec497be2cb0
zhangxun <zhxun0422@gmail.com>
2016-01-06 23:14:50 +0800

    提交Temptest修改

---
+++ TempTest/test.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int i = 100;
+    if (i>10)
+    {
+        cout << 1;
+    }
+    else if(i>20)
+    {
+        cout << 2;
+    }
+
+    system("pause");
+    return 0;
+}
