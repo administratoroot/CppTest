02d3277b9c4e120cff400a79dc752b3a940f0f4e
administratoroot <zhxun0422@gmail.com>
2016-01-21 13:34:16 +0800

    提交共享内存工程文件

--- BitOperate/bitOperate.cpp
+++ BitOperate/bitOperate.cpp
@@ -10,6 +10,9 @@ int main()
 	cout << "(i^j)" << (i ^ j) << endl;//111
 	cout << "(i^k)" << (i ^ k) << endl;//011
 	cout << "(~i)" << (~i) << endl;//
+
+    cout << hex << i << endl;
+    cout << hex << ~i + 1 << " " << -i << endl;
 	system("pause");
 	return 0;
 }
