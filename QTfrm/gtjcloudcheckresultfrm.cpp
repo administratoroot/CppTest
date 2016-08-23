4789d09f453a48c137ef735de6cc2c4681851cc7
administratoroot <zhxun0422@gmail.com>
2016-08-23 10:16:54 +0800

    提交弹对话框，读配置文件demo

--- QTfrm/gtjcloudcheckresultfrm.cpp
+++ QTfrm/gtjcloudcheckresultfrm.cpp
@@ -7,14 +7,32 @@
 #include <QPushButton>
 #include <QWidget>
 #include <QIcon>
+const QString sButtonStyleSheet = "QPushButton{font: 75 12px Microsoft YaHei; color: #333333; background:transparent; border:none;}" \
+"QPushButton:hover{font: 75 12px Microsoft YaHei; color: #00b4ff; background:transparent; border:none;}";
 
 GTJCloudCheckResultFrm::GTJCloudCheckResultFrm(QWidget *parent)
     : QDialog(parent)
 {
     setWindowFlags(Qt::Window /*| Qt::FramelessWindowHint*/);
-    setFixedSize(400, 800);
+    setFixedSize(300, 800);
+
+    QPushButton * m_pRefreshResultBtn = new QPushButton();
+    //m_pRefreshResultBtn->setMinimumWidth(0);
+    m_pRefreshResultBtn->adjustSize();
+    m_pRefreshResultBtn->setText(QString::fromLocal8Bit("刷新"));
+    m_pRefreshResultBtn->setIcon(QIcon("F:/GitRepos/CppTest/Win32/Debug/images/CheckResult/Refresh.png"));
+    m_pRefreshResultBtn->setCursor(Qt::PointingHandCursor);
+    m_pRefreshResultBtn->setStyleSheet(sButtonStyleSheet);
+
+    QPushButton* m_pReviewRuleBtn = new QPushButton();
+    //m_pRefreshResultBtn->setMinimumWidth(0);
+    m_pReviewRuleBtn->setText(QString::fromLocal8Bit("规则设置"));
+    m_pReviewRuleBtn->setIcon(QIcon("F:/GitRepos/CppTest/Win32/Debug/images/CheckResult/FindRule.png"));
+    m_pReviewRuleBtn->setCursor(Qt::PointingHandCursor);
+    m_pReviewRuleBtn->setStyleSheet(sButtonStyleSheet);
 
     QTabWidget* pTabWidget = new QTabWidget(this);
+ 
 
     //新建第一个页面的部件
     QWidget *widget1 = new QWidget();
@@ -23,6 +41,7 @@ GTJCloudCheckResultFrm::GTJCloudCheckResultFrm(QWidget *parent)
     lable->setText("aaa");
     vLayout->addWidget(lable);
     widget1->setLayout(vLayout);
+
     //向QTabWidget中添加第一个页面
     pTabWidget->addTab(widget1, QString::fromLocal8Bit("结果列表"));
 
@@ -40,12 +59,24 @@ GTJCloudCheckResultFrm::GTJCloudCheckResultFrm(QWidget *parent)
     //向QTabWidget中添加第三个页面
     pTabWidget->addTab(widget3, QString::fromLocal8Bit("修复列表"));
 
-    QHBoxLayout *layout = new QHBoxLayout();
-    layout->addWidget(pTabWidget);
 
-    this->setLayout(layout);
+    QHBoxLayout *HLayout1 = new QHBoxLayout();
+    HLayout1->addWidget(m_pRefreshResultBtn);
+    HLayout1->addWidget(m_pReviewRuleBtn);
+    HLayout1->addSpacing(8);
+    HLayout1->addStretch();  // 添加伸缩
+
+    QHBoxLayout *HLayout2 = new QHBoxLayout();
+    HLayout2->addWidget(pTabWidget);
+
+    QVBoxLayout *VLayout = new QVBoxLayout();
+    VLayout->addLayout(HLayout1);
+    VLayout->addLayout(HLayout2);
+    VLayout->setContentsMargins(8, 9, 8, 9);
+    this->setLayout(VLayout);
     this->resize(300, 100);
     this->setWindowTitle(QString::fromLocal8Bit("检查结果"));
+
 }
 
 GTJCloudCheckResultFrm::~GTJCloudCheckResultFrm()
--- QTfrm/main.cpp
+++ QTfrm/main.cpp
@@ -1,10 +1,92 @@
 #include "qtfrm.h"
 #include <QtWidgets/QApplication>
+#include <QDialog>
+#include <QLabel>
+#include <QLayout>
+#include <QTextBrowser>
+#include <QFile>
+#include <QSettings>
+
+//读文件
+void readFeedbackOptions(QString sGroup, bool& bEnable, QString& sUrl)
+{
+    const QString sPath = "C:/test.ini";
+    if (sPath.isEmpty())
+    {
+        bEnable = false;
+        sUrl = QString();
+    }
+    else
+    {
+        //创建配置文件操作对象
+        QSettings *config = new QSettings(sPath, QSettings::IniFormat);
+        
+        //读取用户配置信息
+        if (config->contains(sGroup + "Enable") && config->contains(sGroup + "Url"))
+        {
+            bEnable = config->value(sGroup + "Enable").toBool();
+            sUrl = config->value(sGroup + "Url").toString();
+        }
+        else
+        {
+            bEnable = false;
+            sUrl = QString();
+        }
+    }
+}
+
+//弹框
+void initCloudCheckFeedbackDlg()
+{
+    
+    QString sGroup = QString("CloudCheckFeedbackFrm/");
+    bool bEnable = false;
+    QString sUrl;
+    readFeedbackOptions(sGroup, bEnable, sUrl);
+    if (bEnable)
+    {
+        const QString sFontStyleSheet = "QLabel{font: 75 16px Microsoft YaHei; color: black; background:transparent; border:transparent;}";
+        const QString sFeedbackStyle = "QLabel{font: 75 16px Microsoft YaHei;}";
+        QDialog dialog;
+        dialog.setWindowTitle(QString::fromLocal8Bit("反馈表"));
+        dialog.setFixedSize(400, 200);
+        QVBoxLayout* pVBoxLayout = new QVBoxLayout;
+        QHBoxLayout* pHLayout1 = new QHBoxLayout;
+        QLabel *pTextLabel = new QLabel(QString::fromLocal8Bit("新产品体验完成，填写一下您的感受吧"));
+        pTextLabel->setStyleSheet(sFontStyleSheet);
+        pHLayout1->setContentsMargins(0, 20, 0, 0);
+        pHLayout1->addStretch();
+        pHLayout1->addWidget(pTextLabel);
+        pHLayout1->addStretch();
+
+
+        QHBoxLayout* pHLayout2 = new QHBoxLayout;
+        QLabel* pLinkLabel = new QLabel();
+        pLinkLabel->setOpenExternalLinks(true);
+        pLinkLabel->setText("<a href =" + sUrl + " >" + QString::fromLocal8Bit("反馈表") + "</a>");
+        pLinkLabel->setStyleSheet(sFeedbackStyle);
+        pHLayout2->setContentsMargins(0, 20, 0, 0);
+        pHLayout2->addStretch();
+        pHLayout2->addWidget(pLinkLabel);
+        pHLayout2->addStretch();
+
+        pVBoxLayout->addLayout(pHLayout1);
+        pVBoxLayout->addLayout(pHLayout2);
+
+        pVBoxLayout->addStretch();
+        dialog.setLayout(pVBoxLayout);
+        dialog.exec();
+    }
+    
+}
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    QTfrm w;
-    w.show();
+    //QTfrm w;
+    //w.show();
+
+
+    initCloudCheckFeedbackDlg();
     return a.exec();
 }
